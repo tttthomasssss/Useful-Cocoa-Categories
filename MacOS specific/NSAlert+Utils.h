d2bc2260c68403143074a154ce711013555f198e
tttthomasssss <th0mas.ko6er@gmail.com>
2012-07-25 11:54:23 +0100

    NSAlert category for changing the font of all controls on the alert window

---
+++ MacOS specific/NSAlert+Utils.h
@@ -0,0 +1,44 @@
+//
+//  NSAlert+Utils.h
+//
+//  Created by Thomas Kober on 7/23/12.
+//
+//	The MIT License (MIT)
+//
+// 	Copyright (c) 2012 Thomas Kober
+//	
+//	In a Nutshell:
+//
+//	Do whatever you want with this source, but don't blame me when things go wrong ;)
+//
+//
+// 	Permission is hereby granted, free of charge, to any person obtaining a copy of 
+//	this software and associated documentation files (the "Software"), to deal in the 
+//	Software without restriction, including without limitation the rights to use, copy, 
+//	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+//	and to permit persons to whom the Software is furnished to do so, subject to the 
+//	following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all copies 
+//	or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+//	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+//	FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+//	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+//	DEALINGS IN THE SOFTWARE.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface NSAlert (Utils)
+
+/**
+ * Change the Font of all Controls on the Alert window.
+ *
+ * Note: Its a bit of a (dirty) quickhack, but I think still better than messing about with custom sheets.
+ */
+- (void)setAlertFont:(NSFont *)font;
+
+@end
